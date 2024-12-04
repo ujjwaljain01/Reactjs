d09c215c00097b1006070166f844f5351894481c
Ujjwal jain <ujjwaljainmain@gmail.com>
2024-12-05 00:25:17 +0530

    create a component and display it

---
+++ 01basicreact/src/Name.js
@@ -0,0 +1,7 @@
+function Name(){
+    return (
+        <h1>ujjwal</h1>
+    )
+}
+
+export default Name
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Name from './Name'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Name/>
   </React.StrictMode>
 );
