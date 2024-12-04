d33aeb4de0176165a3a6f4fe781a2a51453c0c3c
Ujjwal jain <ujjwaljainmain@gmail.com>
2024-12-05 00:47:13 +0530

    display component through another component

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,10 @@
-
+import Name from './Name'
 function App() {
   return (
+    <>
+    <Name></Name>
     <h1>Hello</h1>
+    </>
   );
 }
 
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Name from './Name'
+// import Name from './Name'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-    <Name/>
-  </React.StrictMode>
+    /* <Name/> */
 );
